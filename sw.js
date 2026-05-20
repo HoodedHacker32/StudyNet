f8efc5d2af8ed62c3dfc60ac7bf79d390f4375d1
HoodedHacker <elliotactoncarey@gmail.com>
2026-05-20 20:21:37 +0100

    Deploy 2026-05-20 20:21

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE = 'studynet-v7';
+const CACHE = 'studynet-v8';
 const SHELL = [
   './',
   './index.html',
   './irish.html',
   './french.html',
-  './history.html',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
